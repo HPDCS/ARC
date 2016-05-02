131a71057eb74b99cc50fc875b6dc39381dcea49
Mauro Ianni <mauroianni@gmail.com>
2016-05-02 16:45:27 +0000

    done some update to use test functions

---
+++ registerLarsson/main.c
@@ -0,0 +1,154 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <errno.h>
+#include <math.h>
+
+#include "timer.h"
+
+#include "register.h"
+
+struct wf_register *reg;
+unsigned long long val = 1;
+unsigned int busy_write, busy_read,  end_write, end_read, size, count_write=0;
+unsigned int *count_read;
+bool end = false;
+
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = b[i] & (1<<j);
+            byte >>= j;
+            printf("%u", byte);
+        }
+    }
+    puts(" ");
+}
+
+void * run_write(void *args){
+	unsigned char arr[size];
+	unsigned int j=0, k;
+	struct writer_slot *wr_slt;
+	
+	printf("[0]WR: START\n");
+	
+	while(true){
+		//write		
+		reg_write(reg, arr, size);
+		//busy loops
+		for(j=0; j<busy_write*1000; j++){
+			k=j*j;
+		}
+		count_write++;
+		//end condition
+		if((count_write%(end_write/10)) == 0) printf("WR: Iteration %10u\n",count_write);
+		if(count_write >= end_write) break;
+	}
+	
+	end = true;
+	printf("WR: END\n");
+	
+	pthread_exit(NULL);
+
+
+
+}
+
+void * run_read(void *args){
+	unsigned int k,j;
+	char *ll;
+	struct reader_slot *rd_slt; 
+	unsigned int id;
+	rd_slt= reader_init(reg);
+	id = reader_init(reg);
+	printf("[%u]RD: START\n", id);
+	
+	sleep(1);
+
+	while(!end){
+		//read
+		ll=reg_read(reg, id);
+		//busy loops
+		for(j=0; j<busy_read*1000; j++){
+			k=j*j;
+		}
+		count_read[id]++;
+		//end condition
+		//if((count_read[id]%(end_read/5)) == 0) printf("[%u]RD: Iteration %10u\n",id,count_read[id]);
+		//if(count_read[id] >= end_read) break;
+	}
+	printf("[%u]RD: %u read operations performed\n", id,count_read[id]);
+	
+	pthread_exit(NULL);
+
+}
+
+int main(int argn, char *argv[]) {
+	unsigned int i, ret, readers, writers, tot_count_read;
+    timer exec_time;
+
+    if(argn < 3) {
+        fprintf(stderr, "Usage: %s: n_writer n_reader\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+      
+    writers = atoi(argv[1]);
+    readers = atoi(argv[2]);
+    end_write = atoi(argv[3]);
+    end_read = atoi(argv[4]);
+    busy_write = atoi(argv[5]);
+    busy_read = atoi(argv[6]);
+    size = atoi(argv[7]);
+    
+    pthread_t p_tid[writers + readers];    
+	reg = reg_init(writers, readers, 8);
+	//reg = reg_init(writers, readers, sizeof(unsigned long long));
+    
+    if((count_read = calloc(readers, sizeof(unsigned int))) == NULL){
+		printf("malloc failed\n");
+		abort();
+	}
+    
+    printf("START TEST on REGISTER(%u,%u) of size %u:\n", writers, readers, size);
+    printf("\t write operation: %u, %u:\n", end_write, busy_write);
+    printf("\t read  operation: %u, %u:\n", end_read, busy_read);
+  
+    printf("+-----------------------------------------------------------------------------------+\n\n\n");
+
+    timer_start(exec_time);
+    
+    for(i = 0; i < writers; i++) {
+        if( (ret = pthread_create(&p_tid[i], NULL, run_write, NULL)) != 0) {
+            fprintf(stderr, "%s\n", strerror(errno));
+            abort();
+        }
+    }
+    for(i = 0; i < readers; i++) {
+        if( (ret = pthread_create(&p_tid[writers+i], NULL, run_read, NULL)) != 0) {
+            fprintf(stderr, "%s\n", strerror(errno));
+            abort();
+        }
+    }
+    
+    for(i = 0; i < writers + readers; i++){
+        pthread_join(p_tid[i], NULL);
+    }
+
+    printf("\n\n+-----------------------------------------------------------------------------------+\n");
+    printf("TEST ENDED: %.5f seconds\n", (double)timer_value_seconds(exec_time));
+    printf("TOTAL WRTE: %u\n", count_write);
+	for(i = 0; i < readers; i++)  tot_count_read +=count_read[i];
+    printf("TOTAL READ: %u\n", tot_count_read);
+    reg_free(reg);
+}
---
+++ registerLarsson/register.c
@@ -0,0 +1,170 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+#define NRDR_MASK 0X00000000ffffffff	// mask of the reader counter
+#define PTRFIELDLEN 6
+#define PTRFIELD 0X000000000000003f
+
+
+struct register_slot {
+	unsigned int r_started; 	// number of read operation started on the slot
+	unsigned int r_endend; 		// number of read operation ended on the slot
+	unsigned int size;			// size of memory of the data associated to the slot (used for variable size register)
+	void *value_loc; 			// pointer to the register value corresponding to this slot
+};
+
+struct wf_register {
+	unsigned long long current; 		// composed by [32 bit of index ||32 bit of counter] 
+	struct register_slot *rw_space;	// curcular buffer of register slot to use for the current value
+	unsigned int size_slot;			// size of the register. Only for fixed size
+	unsigned int writers;			// max number of concurrent writers, fixed in the init
+	unsigned int readers;			// max number of concurrent readers, fixed in the init
+	unsigned int readers_registered;			// max number of concurrent readers, fixed in the init
+	unsigned int *tracer;
+} ;
+
+void *_reg_write(struct wf_register *reg, void *value, unsigned int size);
+
+/**
+* Init the register allocating the needed amount of memory and calculate the base for the current value.
+*
+* @author Mauro Ianni
+* @param the number of writers and readers, and the size of the register
+* @return a pointer to the instance of the register
+* @date 24/02/2016
+*/
+struct wf_register *_reg_init(unsigned int n_wrts, unsigned int n_rdrs, unsigned int size){	
+	//TODO: prima scrittura a 0 o ad un valore dato	
+	unsigned int i;
+	struct wf_register *reg;
+ 	if((reg = malloc(sizeof(struct wf_register)))==NULL){
+		printf("malloc failed\n");
+        abort();
+	}			
+	reg->readers = n_rdrs;	
+	reg->writers = n_wrts;	
+	reg->size_slot = size;		
+	reg->current = 0;
+	
+	if((reg -> rw_space = calloc(reg->readers+2, sizeof(struct register_slot)))==NULL){
+		printf("malloc failed\n");
+        abort();
+	}
+	
+	if((reg -> tracer = calloc(reg->readers, sizeof(unsigned int)))==NULL){
+		printf("malloc failed\n");
+        abort();
+	}
+	
+	/* if the register is static, the memory is preallocated */
+	if(size != 0){
+		for(i = 0; i < n_rdrs+2; i++){
+			if((reg->rw_space[i].value_loc = malloc(size))==NULL){
+				printf("malloc failed\n");
+				abort();
+			}
+		}
+	}
+	/* if the register is dynamic */
+	
+	_reg_write(reg, &reg->current, 8);/*
+	else{
+		for(i = 0; i < n_rdrs+2; i++){
+			reg->rw_space[i].value_loc = NULL;
+		}
+	}*/
+	
+	return reg;
+}	
+
+unsigned int reader_init(struct wf_register *reg){
+	unsigned int id;
+	id = __sync_fetch_and_add(&reg->readers_registered, 1);
+	if(id >= 58) 
+		return ~0;
+	else 
+		return id;
+}
+
+void *_reg_write(struct wf_register *reg, void *val, unsigned int size){
+	unsigned int tmp_size, tr;
+	unsigned long long current_index, i, j, wsync, oldwptr;
+	
+	current_index = reg->current & PTRFIELD;
+	i = (current_index + 1) % (reg->readers + 2);
+	
+	while(1){
+		if(current_index != i){
+			tr = 0;
+			for(j = 0; j < reg->readers ; j++){
+				if(reg->tracer[j] == i) tr++;
+			}
+		}
+		if(tr == 0) break;
+		i++;
+		i = i % (reg->readers + 2); 
+	}
+	
+
+	tmp_size = reg->rw_space[i].size;
+	memcpy(reg->rw_space[i].value_loc, val, tmp_size);
+	
+	wsync = __sync_lock_test_and_set(&reg->current, i);
+	
+	oldwptr = wsync & PTRFIELD;
+	
+	for(j = 0; j < reg->readers ; j++)
+		if(wsync & (1 << (j + PTRFIELDLEN)))	
+			reg->tracer[j] = oldwptr;
+
+	return reg->rw_space[i].value_loc; 
+}
+
+
+/**
+* Read th value of the register
+*
+* @author Mauro Ianni
+* @param the pointer to the register, the address of the value to write and it's size
+* @return the pointer to a memory location with a copy of the current value
+* @date 24/02/2016
+*/
+void *reg_read(struct wf_register *reg, unsigned int id){
+	unsigned long long readerbit, rsync, rptr;
+	void *mem_loc, *mem_ret;
+	
+	
+	readerbit = 1 << (id + PTRFIELDLEN);
+	rsync = __sync_fetch_and_or(&reg->current, readerbit);
+	rptr = rsync & PTRFIELD;
+	
+	if((mem_ret = malloc(reg->size_slot))==NULL){
+		printf("malloc failed\n");
+        abort();
+	}
+	/* read from the memory location */
+	mem_loc = reg->rw_space[rptr].value_loc;
+	memcpy(mem_ret, mem_loc, reg->rw_space[rptr].size);
+	
+	return mem_ret;
+}
+
+/**
+* Free the memory used by the register
+*
+* @author Mauro Ianni
+* @param the pointer to the register
+* @return void
+* @date 24/02/2016
+*/
+void reg_free(struct wf_register *reg){
+	free(reg->rw_space);
+	free(reg);
+}
+
+
